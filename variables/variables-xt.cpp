0bb00117044c3d410f5faa2a084a3de88b5400c1
sixLUKEY <marcoslukerojo@gmail.com>
2023-12-14 13:22:49 +0200

    variables update

---
+++ variables/variables-xt.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 5, y = 12, z = 43;
+    cout << x + y + z;
+}
+
+//Create constants using const
+const int minutesPerHour = 60;
--- variables/variables.cpp
+++ variables/variables.cpp
@@ -2,7 +2,25 @@
 using namespace std;
 
 int main(){
-    int myNum = 15;
-    myNum = 10;
-    cout << myNum;
+    int myNum = 15; //myNum is 15
+    myNum = 10; //myNum is changed to 10
+    cout << myNum; //outputs 10
+    int x = 5;
+    int y = 8;
+    int sum = x + y;
+    cout << "\n";
+    cout << sum;
 }
+
+// Some more variable examoles
+int myNum = 5;
+double myFloatNum = 5.99;
+char myLetter = 'k';
+string myText = "Hello";
+bool myBoolean = true;
+
+//Display Variables 
+// int main(){
+//     int myAge = 35;
+//     cout << "I am" << myAge << "years old.";
+// }
